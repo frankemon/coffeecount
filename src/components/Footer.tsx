f2bb913025494caa62347a2dd6f199eef7927625
frankemon <frankie.hugo@hotmail.com>
2024-11-23 11:58:15 +0100

    add date fx output to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <div className=" bottom-0 w-full border-t-2 p-2 border-primary font-bold bg-secondary">
       <footer className="flex justify-center items-center">
-        <p>© 2024 CoffeeCount. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} CoffeeCount. All rights reserved.</p>
       </footer>
     </div>
   );
